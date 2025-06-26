1e45a3035d736d1fbdf3abef64c04ed1e05b30ad
lucasgontijo13 <lucasgontijo111@gmail.com>
2025-06-26 14:22:04 -0300

    first commit

---
+++ src/test/java/br/edu/ifmg/locadora/LocadoraApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifmg.locadora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocadoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
